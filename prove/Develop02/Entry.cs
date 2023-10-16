82c94ef8e6c37a4302dac2670626bbaa1acc6858
guidarmike <gui23004@byui.edu>
2023-10-16 11:10:00 +0200

    Update for files-classes

---
+++ prove/Develop02/Entry.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+class Entry
+{
+    public string Prompt { get; set; }
+    public string Response { get; set; }
+    public string Date { get; set; }
+    public string Location { get; set; } // Creativity: property for location
+    public string Affirmation { get; set; } // Creativity: property for affirmation
+}
